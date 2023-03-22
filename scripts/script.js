7f450933921ad2a4d96e8fcf5a85fc3f3af0d363
Vladimir Savchook <savchookvladimir@yandex.ru>
2023-03-22 19:40:20 +0500

    clean up

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,23 @@
 let popup = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__close-button');
-let editButton = document.querySelector('.profile__edit-button');
 
 let userName = document.querySelector('.profile__user-name');
 let userProfession = document.querySelector('.profile__user-profession');
+let editButton = document.querySelector('.profile__edit-button');
 
+let formElement = document.querySelector('.form');
 let userNameInput = document.querySelector('#user-name');
 let userProfessionInput = document.querySelector('#user-profession');
 
 function openPopup() {
-  popup.classList.add('popup_opened');
-
   userNameInput.setAttribute('value', userName.textContent);
   userProfessionInput.setAttribute('value', userProfession.textContent);
+
+  popup.classList.add('popup_opened');
 };
-
-popupCloseButton.addEventListener('click', function () {
-  popup.classList.remove('popup_opened');
-});
-
-editButton.addEventListener('click', openPopup);
-
-
-
-
-let formElement = document.querySelector('.form');
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
-
-  userNameInput = document.querySelector('#user-name');
-  userProfessionInput = document.querySelector('#user-profession');
 
   userName.textContent = userNameInput.value;
   userProfession.textContent = userProfessionInput.value;
@@ -39,3 +26,9 @@ function handleFormSubmit (evt) {
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
+
+popupCloseButton.addEventListener('click', function () {
+  popup.classList.remove('popup_opened');
+});
+
+editButton.addEventListener('click', openPopup);
